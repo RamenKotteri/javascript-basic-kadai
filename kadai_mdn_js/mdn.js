62631d6ab2d038c34633ba8ed11c79c88eacd872
RamenKotteri <78hisya@gmail.com>
2025-07-12 11:42:16 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,1 +1,8 @@
-console.log(Date());
+const date = new Date(Date());
+const [month, day, year] = [
+  date.getMonth(),
+  date.getDate(),
+  date.getFullYear(),
+];
+let m= date.getMonth()+1;
+console.log(date.getFullYear(), m, date.getDate());
